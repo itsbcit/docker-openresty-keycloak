d226993e20f93ef45b07f2ea9c0a645877e95eec
Jesse Weisner <jesse@weisner.ca>
2020-11-16 16:08:27 -0800

    Fixed missing semicolons

--- devel/60-set-keycloak-env.sh
+++ devel/60-set-keycloak-env.sh
@@ -5,20 +5,20 @@ kc_config="/usr/local/openresty/nginx/conf.d/01-keycloak.conf"
 
 # check for "keycloak_endpoint" already set
 if (! grep -q "^set keycloak_endpoint" );then
-    echo "set keycloak_endpoint ${KEYCLOAK_ENDPOINT}" >> $kc_config
+    echo "set keycloak_endpoint ${KEYCLOAK_ENDPOINT};" >> $kc_config
 fi
 
 # check for "keycloak_realm" already set
 if (! grep -q "^set keycloak_realm" );then
-    echo "set keycloak_realm ${KEYCLOAK_REALM}" >> $kc_config
+    echo "set keycloak_realm ${KEYCLOAK_REALM};" >> $kc_config
 fi
 
 # check for "keycloak_client_id" already set
 if (! grep -q "^set keycloak_client_id" );then
-    echo "set keycloak_client_id ${KEYCLOAK_CLIENT_ID}" >> $kc_config
+    echo "set keycloak_client_id ${KEYCLOAK_CLIENT_ID};" >> $kc_config
 fi
 
 # check for "keycloak_client_secret" already set
 if (! grep -q "^set keycloak_client_secret" );then
-    echo "set keycloak_client_secret ${KEYCLOAK_CLIENT_SECRET}" >> $kc_config
+    echo "set keycloak_client_secret ${KEYCLOAK_CLIENT_SECRET};" >> $kc_config
 fi
